65af74032a5cfafc9557903a51f079a602adbdc7
aniket sharma <aniket965.as@gmail.com>
2017-10-19 11:45:48 +0530

    snflksnfs

--- basicTranformations/Scaling.py
+++ basicTranformations/Scaling.py
@@ -1,1 +1,21 @@
-# Interpolations
+# Scaling
+import cv2
+import numpy as np
+image = cv2.imread('owl.jpg')
+
+scaled_image = cv2.resize(image, None, fx = 0.75, fy = 0.75)
+cv2.imshow('Scaled image ', scaled_image)
+cv2.waitKey()
+
+# Interpolated Image
+img_scaled = cv2.resize(image, (900,400), interpolation = cv2.INTER_CUBIC)
+cv2.imshow('interpolated image', img_scaled)
+
+# Image Pyramids 
+## Doubles a image size or decrease the Image Size by half
+img_pyr_down = cv2.pyrDown(image)
+cv2.imshow('Sequezed using Pyramid', img_pyr_down)
+
+cv2.waitKey()
+
+cv2.destroyAllWindows()
